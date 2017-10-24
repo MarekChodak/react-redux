c6a2f841058dd248fda0691acd5a6012c401f3df
marekchodak <marekchodak@hotmail.com>
2017-10-24 21:48:44 +0200

    Manage course

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import HomePage from './home/HomePage';
 import Header from './common/Header';
 import AboutPage from './about/AboutPage';
 import CoursesPage from './course/CoursesPage';
+import ManageCoursePage from './course/ManageCoursePage'
 
 class App extends React.Component{
 
@@ -16,6 +17,8 @@ class App extends React.Component{
           <Route exact path="/" component={HomePage}/>
           <Route path="/about" component={AboutPage}/>
           <Route path="/courses" component={CoursesPage}/>
+          <Route path="/course" component={ManageCoursePage}/>
+          <Route path="/course:id" component={ManageCoursePage}/>
         </div>
       </Router>
     );
---
+++ src/components/course/ManageCoursePage.js
@@ -0,0 +1,47 @@
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux'
+import * as courseActions from '../../actions/courseActions';
+import CourseForm from './CourseForm'
+
+class ManageCoursePage extends React.Component {
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      course : Object.assign({}, props.course),
+      errors:{}
+    }
+  }
+
+  render(){
+    return (
+      <CourseForm
+        allAuthors={[]}
+        errors={this.state.errors}
+        course={this.state.course}/>
+    );
+  }
+}
+
+ManageCoursePage.PropTypes = {
+  course: PropTypes.object.isRequired
+};
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions : bindActionCreators(courseActions, dispatch)
+  };
+}
+
+function mapStateToProps(state, ownProps) {
+
+  let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''};
+
+  return {
+    course: course
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
